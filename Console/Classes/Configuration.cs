d2bb0746fb9a07db43296a3bb3ad49e372e3df28
Nam <nam@specialmoves.com>
2013-09-03 16:44:31 +0100

    Initial commit

---
+++ Console/Classes/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Linq;
+
+namespace Console.Classes
+{
+    public class ConfigurationHelper
+    {
+        public static string GetAppSetting(string key)
+        {
+            return GetAppSetting(key, true);
+        }
+
+        public static string GetAppSetting(string key, bool required)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(value) && required)
+            {
+                return string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                value = ConfigurationManager.AppSettings.AllKeys.Aggregate(value, (current, k) => current.Replace("{" + k + "}", ConfigurationManager.AppSettings[k]));
+            }
+
+            return value;
+        }
+    }
+}
